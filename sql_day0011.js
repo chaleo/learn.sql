98a60c5c7f39f8355f732af14ef6585c6e05f508
Nguyen Le <117344724+chaleo@users.noreply.github.com>
2023-01-05 17:54:34 +0700

    Update sql_day0011.js

--- sql_day0011.js
+++ sql_day0011.js
@@ -6,5 +6,14 @@ WHERE Country='Mexico';
 SELECT * FROM Customers
 WHERE CustomerID like "%1";
 
+// =	Equal	
+// >	Greater than	
+// <	Less than	
+//  >=	Greater than or equal	
+// <=	Less than or equal	
+//  <>	Not equal. Note: In some versions of SQL this operator may be written as !=	
+// BETWEEN	Between a certain range	
+// LIKE	Search for a pattern	
+//  IN	To specify multiple possible values for a column
 
 
